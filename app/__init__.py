bb023e21c877b56e397ef1470b8766a89f931d46
skipcheru <sammy.cheruiyot@andela.com>
2017-01-31 14:58:34 +0300

    FIX: register jwt to app in application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_jwt import JWT
 
 db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    jwt = JWT(app, authenticate, identity)
+
     db.init_app(app)
+    jwt.init_app(app)
 
     from .auth.views import auth
     app.register_blueprint(auth)
@@ -17,3 +23,7 @@ def create_app(config_name):
     app.register_blueprint(bucketlists)
 
     return app
+
+from .auth.validate import authenticate, identity, jwt
+
+jwt = JWT(create_app('default'), authenticate, identity)
