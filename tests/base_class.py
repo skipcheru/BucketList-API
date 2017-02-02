71ddd21f191f63e2ddfceea1deaaacc8081a0ca8
skipcheru <sammy.cheruiyot@andela.com>
2017-02-02 14:36:55 +0300

    TEST: create new bucketlist and bucketlist item to be asserted in tests

--- tests/base_class.py
+++ tests/base_class.py
@@ -13,17 +13,30 @@ class TestBase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        # register user and use the toke for authorization
+        # register user and use the token for authorization
         data = json.dumps({'username': 'kiki', 'password': 'shesheni'})
         url, mime_type = '/api/v1/auth/register', 'application/json'
         response = self.client.post(url, data=data, content_type=mime_type)
 
         # Login and get access_token
         data = json.dumps({'username': 'kiki', 'password': 'shesheni'})
-        url, mime_type = '/api/v1/auth/login', 'application/json'
+        url = '/api/v1/auth/login'
         response = self.client.post(url, data=data, content_type=mime_type)
         token = json.loads(response.data)
         self.headers = {'Authorization': 'JWT ' + token['access_token']}
+
+        # create new bucketlist
+        data = json.dumps({'name': 'BucketList A'})
+        msg, url = 'BucketList added successfully', 'api/v1/bucketlists/'
+
+        self.client.post(
+            url, data=data, headers=self.headers, content_type=mime_type)
+
+        # create bucketlist item
+        data = json.dumps({'name': 'The Item'})
+        url = 'api/v1/bucketlists/1/items/'
+        self.client.post(url, data=data, headers=self.headers,
+                         content_type=mime_type)
 
     def tearDown(self):
         db.session.remove()
