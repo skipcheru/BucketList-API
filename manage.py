4ba00f4bde54f1a759e3f2ba3cd11d22998c9338
skipcheru <sammy.cheruiyot@andela.com>
2017-02-09 21:45:42 +0300

    FIX: remove test command

--- manage.py
+++ manage.py
@@ -17,45 +17,5 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
-@manager.command
-def dropdb():
-    if prompt_bool("Are you sure you want to drop db?"):
-        db.drop_all()
-
-
-@manager.command
-def initdb():
-    db.create_all()
-
-
-cov = None
-if os.environ.get('FLASK_COVERAGE'):
-    import coverage
-    cov = coverage.coverage(
-        branch=True, include='app/*?views.py', omit='app/models.py')
-    cov.start()
-
-
-@manager.command
-def test(coverage=False):
-    """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
-        import sys
-        os.environ['FLASK_COVERAGE'] = '1'
-        os.execvp(sys.executable, [sys.executable] + sys.argv)
-
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    if cov:
-        cov.stop()
-        cov.save()
-        print('Coverage Summary:')
-        cov.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        cov.html_report(directory=covdir)
-        cov.erase()
-
 if __name__ == '__main__':
     manager.run()
